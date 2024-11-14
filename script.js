32bea0b922b6035f9f8841982d942712d49639cd
Eliry <123857622+Eliry@users.noreply.github.com>
2024-11-14 09:59:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -82,10 +82,11 @@ function handleNumber(numberString) {
     }
 }
 
+
 function init() {
     document.querySelector('.calc-buttons').addEventListener('click', function(event) {
         buttonClick(event.target.innerText);
     })
 }
 
-init();
+init
